1a58cacbfc7f4cd8bc7d88ddf3077f4e5655f32b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-03 14:50:19 +0000

    add admin pages for countries

---
+++ app/controllers/admin/countries_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CountriesController < Comfy::Admin::Cms::BaseController
+  def index
+    @countries = Country.all
+  end
+
+  def edit
+    @country = Country.find(params[:id])
+  end
+
+  def update
+    @country = Country.find(params[:id])
+    @country.update(country_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:iso_3, :name)
+  end
+end
