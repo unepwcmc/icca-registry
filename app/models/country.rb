6ce79318b0cda8dd79cebc3237e4d4ff046dcbf0
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-03 14:51:58 +0000

    update page label if country name is changed

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,15 @@
 class Country < ActiveRecord::Base
   has_many :icca_sites
   has_one :page, class_name: "Comfy::Cms::Page"
+
+  after_save :update_cms_page
+
+  private
+
+  def update_cms_page
+    if page
+      page.label = name
+      page.save
+    end
+  end
 end
