5343fa0bd7ce2f54044450f9bd6c6365f810b436
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-06-18 15:28:23 +0100

    add link to CMS page in sites admin page

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -16,4 +16,18 @@ module CmsHelper
   def cms_page_path page
     File.join('/', page.site.path, page.full_path)
   end
+
+  def link_to_cms_page icca_site, site
+    page = icca_site.pages.where(site_id: site.id).first
+
+    if page
+      link_to(
+        "Edit CMS page",
+        edit_comfy_admin_cms_site_page_path(site_id: site, id: page.id),
+        class: "btn btn-default"
+      )
+    else
+      ""
+    end
+  end
 end
