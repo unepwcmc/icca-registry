85660c388849fa53b76ca7e831b9108e8f005398
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-05-05 13:43:54 +0100

    add French translations to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :load_locale
   before_filter :load_cms_pages
+
+  def load_locale
+    I18n.locale = params[:locale] || "en"
+  end
 
   def load_cms_pages
     pages = Comfy::Cms::Page
