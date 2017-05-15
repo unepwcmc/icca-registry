a2284971a5c62944682d97cec8e75370bd55e5dd
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-05-15 10:00:33 +0100

    add protect_from_forgery in interest_submissions_controller

--- app/controllers/api/interest_submissions_controller.rb
+++ app/controllers/api/interest_submissions_controller.rb
@@ -1,4 +1,6 @@
 class Api::InterestSubmissionsController < ApplicationController
+  protect_from_forgery
+
   def create
     InterestSubmission.create(
       params.permit(:icca_name, :icca_size, :country, :can_contact, :email)
