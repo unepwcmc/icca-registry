4619c8dc88164573d1f9bd4cc16c855bc0f1f6e4
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-05-17 11:15:28 +0100

    preload pages and sites for icca sites in api endpoint

--- app/controllers/api/icca_sites_controller.rb
+++ app/controllers/api/icca_sites_controller.rb
@@ -1,7 +1,7 @@
 class Api::IccaSitesController < ApplicationController
   def index
-    render json: IccaSite.all.as_json(include: {
-      pages: {include: :site, only: [:label, :full_path]}
-    })
+    render json: IccaSite.includes(pages: :site).all.as_json(
+      include: {pages: {include: :site, only: [:label, :full_path]}}
+    )
   end
 end
