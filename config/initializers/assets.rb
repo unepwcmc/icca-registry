ee356baea4885891b2652957cb2a3b07d6f59167
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-05-22 14:25:06 +0100

    complete translation on interest modal and banner

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'protectedplanet-frontend/dist/style')
 Rails.application.config.assets.precompile += %w(html5shiv/dist/*)
+Rails.application.config.assets.precompile += %w(locales/*)
 
 Sprockets.register_preprocessor('text/css',
   AssetJoiner.new('stylesheets/overrides.scss', 'protectedplanet-frontend/src/style/main')
