7d786fc3058abc257ca9c9d28072346227c4397d
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-11-09 12:51:13 +0000

    Fixed the french site at least

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @last_case_study = Photo.order("created_at DESC").first.page
+    @photo            = Photo.order("created_at DESC").first
+    @last_case_study  = @photo.page
+    @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
   end
 end
