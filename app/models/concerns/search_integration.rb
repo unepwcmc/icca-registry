7bfa18eec763ef7ef13f1b2dbbe2eafd105affd7
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-03 15:57:01 +0100

    testing railties order

--- app/models/concerns/search_integration.rb
+++ app/models/concerns/search_integration.rb
@@ -1,22 +1,22 @@
-module SearchIntegration
-  module ForFragments
-    extend ActiveSupport::Concern
-    included do
-      include PgSearch
-      multisearchable against: :content, if: -> (record) { record.identifier == "content" }
-    end
-  end
-
-  module ForPages
-    extend ActiveSupport::Concern
-    included do
-      include PgSearch
-      multisearchable against: :label
-    end
+module SearchForPages
+  extend ActiveSupport::Concern
+  included do
+    include PgSearch
+    multisearchable against: :label
   end
 end
 
-Comfy::Cms::Page.send(:include, SearchIntegration::ForPages)
 
-# Search integration for fragments is broken
-# Comfy::Cms::Fragment.send(:include, SearchIntegration::ForFragments)
+module FragmentSearch
+  extend ActiveSupport::Concern
+  included do
+    include PgSearch
+    multisearchable against: :content, if: -> (record) { record.identifier == "content" }
+  end
+end
+
+
+Comfy::Cms::Page.send(:include, SearchForPages)
+
+# Search integration not working at the moment
+# Comfy::Cms::Fragment.send(:include, FragmentSearch)
--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require "active_storage/engine"
+require "active_storage"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,7 +25,6 @@ module IccaRegistry
     config.autoload_paths += %W(#{config.root}/lib/modules)
     config.autoload_paths += %W(#{config.root}/app/presenters)
 
-
     # Rails 5 now supports per-form CSRF tokens to mitigate against code-injection attacks with forms created by JavaScript.
     # With this option turned on, forms in your application will each have their own CSRF token that is specific to the action and method for that form.
     config.action_controller.per_form_csrf_tokens = true
@@ -36,5 +35,7 @@ module IccaRegistry
 
     config.active_record.belongs_to_required_by_default = true
 
+    # Stop CMS globbing so that ActiveStorage routes now become available
+    # config.railties_order = [:all, ActiveStorage::Engine, :main_app]
   end
 end
