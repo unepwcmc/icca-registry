70e20ab9a21550c58e5ef6c229c39f09cf61b3be
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-05 15:01:14 +0100

    added a disclaimer not to use instance method if using local ActiveStorage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   def index
+    pages = Comfy::Cms::Page
+    site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
+
+    @explore_page     = pages.find_by_slug_and_site_id("explore", site.id)
+
     # Website breaks if there are photos without attachments - so @photo defaults to the latest safe option
-    @photo            = ActiveStorage::Attachment.where(record_type: 'Photo').last.record
     @last_case_study  = @photo.page
+    @photo            = ActiveStorage::Attachment.where(record_type: 'Photo').last.record
     @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,7 @@ class Photo < ApplicationRecord
 
   belongs_to :page, class_name: "Comfy::Cms::Page", optional: true
 
+  # Only use if your ActiveStorage service is set to :local - this will give you the local file path
   def file_path
     ActiveStorage::Blob.service.send(:path_for, file.key)
   end
