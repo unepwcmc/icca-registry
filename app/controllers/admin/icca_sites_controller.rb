35c584a1ef749bdd42a0763ed76d634d7f7a410a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-13 21:26:38 +0100

    added a destroy method for icca sites, and added a few keys in the yml to try to fix the localisation issue

--- app/controllers/admin/icca_sites_controller.rb
+++ app/controllers/admin/icca_sites_controller.rb
@@ -1,20 +1,33 @@
 class Admin::IccaSitesController < Comfy::Admin::Cms::BaseController
+  before_action :set_icca_site, only: [:edit, :update, :destroy]
+
   def index
     @icca_sites = IccaSite.order(:name).all
   end
 
   def edit
-    @icca_site = IccaSite.find(params[:id])
   end
 
   def update
-    @icca_site = IccaSite.find(params[:id])
     @icca_site.update(icca_site_params)
 
     redirect_to action: :index
   end
 
+  def destroy
+    if @icca_site.destroy
+      flash[:notice] = 'Site deleted.'
+      redirect_to action: :index
+    else
+      flash[:notice] = @icca_site.errors.full_messages
+    end
+  end
+
   private
+
+  def set_icca_site
+    @icca_site = IccaSite.find(params[:id])
+  end
 
   def icca_site_params
     params.require(:icca_site).permit(:lat, :lon, :name)
--- app/models/icca_site.rb
+++ app/models/icca_site.rb
@@ -1,4 +1,15 @@
 class IccaSite < ApplicationRecord
   belongs_to :country
-  has_many :pages, class_name: "Comfy::Cms::Page"
+  has_many :pages, class_name: "Comfy::Cms::Page", dependent: :destroy
+  before_destroy :safe_to_destroy
+
+  private
+
+  def safe_to_destroy
+    if pages.any?
+      errors[:base] << "Attempted to delete site, but pages found associated with that site. Delete those pages and try again."
+      false
+    end
+  end
+
 end
