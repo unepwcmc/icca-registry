cebc00bb3e4a6954553e28d92bb31f14e5357349
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-15 17:10:47 +0100

    paperclip columns left in for now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_092959) do
+ActiveRecord::Schema.define(version: 2020_05_29_083247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -200,6 +200,10 @@ ActiveRecord::Schema.define(version: 2020_06_03_092959) do
     t.integer "page_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
+    t.datetime "file_updated_at"
   end
 
   create_table "related_links", id: :serial, force: :cascade do |t|
@@ -214,6 +218,10 @@ ActiveRecord::Schema.define(version: 2020_06_03_092959) do
     t.integer "page_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
+    t.datetime "file_updated_at"
     t.string "label"
   end
 
