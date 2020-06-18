0c0ed4bb0855eee9774d82907ddcf8a783d6737a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-18 12:09:28 +0100

    added validation into photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ApplicationRecord
   # has_attached_file :file
 
   has_one_attached :file
-  validates :file, attached: true
+  validates :file, attached: true, content_type: ["image/jpeg", "image/jpg", "image/png"]
 
   belongs_to :page, class_name: "Comfy::Cms::Page", optional: true
 
