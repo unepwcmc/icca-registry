3ff52f6a77d4b0dd4a8a2e5eebb5a961635ffe82
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-18 12:51:32 +0100

    testing the S3 bucket for file uploads

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -35,8 +35,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
@@ -79,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.active_storage.service = :staging
+  config.active_storage.service = :staging
 end
