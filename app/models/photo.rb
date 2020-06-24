7c047ab5e6c6e7d1703aa638fd55190548e1fb7e
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-24 12:59:24 +0100

    updated instance method to access local locations of ActiveStorage files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,13 +1,16 @@
 class Photo < ApplicationRecord
-  has_attached_file :file
+  # has_attached_file :file
 
-  # has_one_attached :file
-  # validates :file, attached: true, content_type: ["image/jpeg", "image/jpg", "image/png"]
+  has_one_attached :file
+  validates :file, attached: true, content_type: ["image/jpeg", "image/jpg", "image/png"]
 
   belongs_to :page, class_name: "Comfy::Cms::Page", optional: true
 
+
   # Only use if your ActiveStorage service is set to :local - this will give you the local file path
+  # for a particular Photo
   def file_path
-    ActiveStorage::Blob.service.send(:path_for, file.key)
+    file_key = self.file.key
+    ActiveStorage::Blob.service.send(:path_for, file_key)
   end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,8 +6,11 @@ class Resource < ApplicationRecord
 
   belongs_to :page, class_name: "Comfy::Cms::Page", optional: true
 
+
   # Only use if your ActiveStorage service is set to :local - this will give you the local file path
+  # for a particular Resource
   def file_path
-    ActiveStorage::Blob.service.send(:path_for, file.key)
+    file_key = self.file.key
+    ActiveStorage::Blob.service.send(:path_for, file_key)
   end
 end
