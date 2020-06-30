7cc2a8840b61820b2ac6be6ef21e0ff1bab83db7
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-30 15:33:55 +0100

    removed references to html5shiv and protectedplanet-frontend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,5 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w(html5shiv/dist/*)
 Rails.application.config.assets.precompile += %w(locales/*)
 
-Sprockets.register_preprocessor('text/css',
-  AssetJoiner.new('stylesheets/overrides.scss', 'protectedplanet-frontend/src/style/main')
-)
