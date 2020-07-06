ae14b7d2a9d54947c9f05fdc5987620600231308
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-06 12:34:41 +0100

    added logic in home controller to handle the issue of no photos for a case study

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,14 @@ class HomeController < ApplicationController
   def index
     # Website breaks if there are photos without attachments - so @photo defaults to the latest safe option
     site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
-    @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).last
-    @photo            = ActiveStorage::Attachment.where(record_type: 'Photo').find do |photo| 
-                          photo.record.page_id == @last_case_study.id 
-                        end.record
+    @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).last  
+    @photo            = find_last_photo.record unless find_last_photo.nil?
     @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
+  end
+
+  def find_last_photo
+    ActiveStorage::Attachment.where(record_type: 'Photo').find do |photo| 
+      photo.record.page_id == @last_case_study.id 
+    end
   end
 end
