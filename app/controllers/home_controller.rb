2ea5376734add8e1a327172faac4c77fe1bf2c24
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-06 11:18:51 +0100

    case studies will only show now if published is explicitly ticked

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,11 @@
 class HomeController < ApplicationController
   def index
-    # pages = Comfy::Cms::Page
-    # site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
-
-    # @explore_page     = pages.find_by_slug_and_site_id("explore", site.id)
-
     # Website breaks if there are photos without attachments - so @photo defaults to the latest safe option
-    @photo            = ActiveStorage::Attachment.where(record_type: 'Photo').last.record
-    @last_case_study  = @photo.page
+    site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
+    @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).last
+    @photo            = ActiveStorage::Attachment.where(record_type: 'Photo').find do |photo| 
+                          photo.record.page_id == @last_case_study.id 
+                        end.record
     @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
   end
 end
