22be1b552bf9873a18b12ab538924748601e55e6
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-07 18:01:45 +0100

    rewrote controller logic to only look for any photos attached to the latest case study

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,8 @@ class HomeController < ApplicationController
     # Website breaks if there are photos without attachments - so @photo defaults to the latest safe option
     site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
     @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).last  
-    @photo            = find_last_photo.record unless find_last_photo.nil?
+    @photo            = @last_case_study.photos.first if @last_case_study.photos.any?
     @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
   end
 
-  private 
-  
-  def find_last_photo
-    ActiveStorage::Attachment.where(record_type: 'Photo').find do |photo| 
-      photo.record.page_id == @last_case_study.id 
-    end
-  end
 end
