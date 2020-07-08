138c9229ae7b06d69297d9a808b73d5a75b4f3ee
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-08 10:40:28 +0100

    corrected for lack of icca sites

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # Website breaks if there are photos without attachments - so @photo defaults to the latest safe option
     site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
-    @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).last  
+    @last_case_study  = Comfy::Cms::Page.where(is_published: true, site_id: site.id).where.not(icca_site_id: nil).last  
     @photo            = @last_case_study.photos.first if @last_case_study.photos.any?
     @icca_site        = IccaSite.find(@last_case_study.icca_site_id)
   end
