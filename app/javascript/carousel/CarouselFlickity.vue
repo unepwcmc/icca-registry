96af6de899d2eacb8ba91d79912e2b3197d8444a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-14 11:46:07 +0100

    placed vue carousel into its own components folder

---
+++ app/javascript/carousel/CarouselFlickity.vue
@@ -0,0 +1,36 @@
+<template>
+  <flickity 
+    :options="flickityOptions"
+    ref="flickity"
+  >
+    <slot />
+  </flickity>
+</template>
+
+<script>
+import Flickity from "vue-flickity";
+
+export default {
+  name: "CarouselFlickity",
+  components: { Flickity },
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: true,
+        pageDots: true,
+        wrapAround: true
+      }
+    };
+  },
+  methods: {
+    next() {
+      this.$refs.flickity.next();
+    },
+
+    previous() {
+      this.$refs.flickity.previous();
+    }
+  }
+};
+</script>
