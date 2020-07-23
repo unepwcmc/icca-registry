4b2909d58937f7d16c5974e113bb3077cf29beaf
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-23 10:40:58 +0100

    no logging needed, and callback not required as automatically redirected back to homepage

--- app/controllers/api/interest_submissions_controller.rb
+++ app/controllers/api/interest_submissions_controller.rb
@@ -1,18 +1,13 @@
 class Api::InterestSubmissionsController < ApplicationController
-  invisible_captcha only: [:create], honeypot: :last_name, on_spam: :spam_caught
+  invisible_captcha only: [:create], honeypot: :last_name
   protect_from_forgery with: :exception
 
   def create
     submission = InterestSubmission.create(params.permit(:icca_name, :icca_size, :country, :can_contact, :email))
 
-    SubmissionMailer.create(submission).deliver_now
+    SubmissionMailer.create(submission).deliver_nows
     head 201
   end
 
   private 
-
-
-  def spam_caught
-    redirect_to root_path
-  end
 end
--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -2,7 +2,3 @@ InvisibleCaptcha.setup do |config|
     config.visual_honeypots = false
 end
 
-ActiveSupport::Notifications.subscribe('invisible_captcha.spam_caught') do |spam|
-    captured_spam = spam.extract_options!
-    Rails.logger.info("#{captured_spam} caught!")        
-end
