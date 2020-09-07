a3ceb843b786ceb8332cb50a0089ed589b481be4
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-07 17:36:34 +0100

    added helpers from PP, no-image svg and yaml key for view all link (other articles partial)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,20 @@ module ApplicationHelper
     return image unless image.blank?
     image_path('hero_image_news-and-stories.jpg')
   end
+
+  def get_news_items all = false
+    news_page = @cms_site.pages.find_by_slug('news-and-stories')
+    published_pages = news_page.children.published
+    sorted_cards = published_pages.sort_by { |c| c.fragments.where(identifier: 'published_date').first.datetime }.reverse
+
+    @items = {
+      title: news_page.label,
+      url: all ? false : get_cms_url(news_page.full_path),
+      cards: all ? sorted_cards : sorted_cards.first(2)
+    }
+  end
+
+  def get_cms_url path
+    root_path + path
+  end
 end
