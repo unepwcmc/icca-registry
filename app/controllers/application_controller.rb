db9da3336852c7d2b4afe9583a8d4ffb276c6189
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-07 10:11:09 +0100

    added news and stories link on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::Base
     site  = Comfy::Cms::Site.find_by_locale(I18n.locale)
 
     @explore_page     = pages.find_by_slug_and_site_id("explore", site.id)
+    @news_page        = pages.find_by_slug_and_site_id("news-and-stories", site.id)
     @contact_us_page  = pages.find_by_slug_and_site_id("contact-us", site.id)
     @about_page       = pages.where(slug: "about", site: site).includes(:children).first
     @participate_page = pages.where(slug: "participate", site: site).includes(:children).first
