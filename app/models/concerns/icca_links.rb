9e4462e04e0b2597d11591337f00da1a3e11f9ef
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-10 15:00:53 +0100

    moved concerns for icca sites and countries into the patch

--- app/models/concerns/icca_links.rb
+++ app/models/concerns/icca_links.rb
@@ -1,48 +1,7 @@
 module IccaLinks
   extend ActiveSupport::Concern
 
-  included do
-    belongs_to :country, optional: true
-    belongs_to :icca_site, optional: true, dependent: :destroy
 
-    has_many :photos
-    has_many :resources
-    has_many :related_links
-
-    before_save :link_icca_site, dependent: :destroy
-    before_save :link_country, dependent: :destroy
-
-    def link_icca_site
-      return true if self.parent.try(:country_id).nil?
-
-      if self.icca_site 
-        IccaSite.where(id: self.icca_site.id).update(name: self.label)
-        normalised_label = I18n.transliterate(self.label)
-        self.slug = normalised_label.downcase.split.join('-')
-      else
-        self.icca_site = IccaSite.find_or_create_by(
-          name: self.label,
-          country: self.parent.country
-        )
-      end
-    end
-
-    def link_country
-      return true if self.parent.try(:label) != "Explore"
-
-      country = Country.find_or_create_by(name: self.label)
-      self.country = country
-
-      self.children.each do |child|
-        icca_site = IccaSite.find_or_create_by(name: child.label)
-        icca_site.country = country
-        icca_site.save
-
-        child.icca_site = icca_site
-        child.save
-      end
-    end
-  end
 
 end
 
--- config/initializers/comfy_monkey_patch.rb
+++ config/initializers/comfy_monkey_patch.rb
@@ -7,11 +7,48 @@ Rails.configuration.to_prepare do
         include PgSearch
 
         multisearchable against: :label
+
+        belongs_to :country, optional: true
+        belongs_to :icca_site, optional: true, dependent: :destroy
+        
+        before_save :link_icca_site, dependent: :destroy
+        before_save :link_country, dependent: :destroy
+    
+        def link_icca_site
+            return true if self.parent.try(:country_id).nil?
+    
+            if self.icca_site 
+                IccaSite.where(id: self.icca_site.id).update(name: self.label)
+                normalised_label = I18n.transliterate(self.label)
+                self.slug = normalised_label.downcase.split.join('-')
+            else
+                self.icca_site = IccaSite.find_or_create_by(
+                    name: self.label,
+                    country: self.parent.country
+                )
+            end
+        end
+    
+        def link_country
+            return true if self.parent.try(:label) != "Explore"
+    
+            country = Country.find_or_create_by(name: self.label)
+            self.country = country
+    
+            self.children.each do |child|
+                icca_site = IccaSite.find_or_create_by(name: child.label)
+                icca_site.country = country
+                icca_site.save
+        
+                child.icca_site = icca_site
+                child.save
+            end
+        end
     end
 
     Comfy::Cms::Fragment.class_eval do
         include PgSearch
-        
+
         multisearchable against: :content, if: -> (record) { record.identifier == "content" }
     end
 end
