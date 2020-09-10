6a5cffe11f93d9c148ea7bc4860d22d62c5a53f4
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-10 14:42:43 +0100

    included pgsearch for pages as well in monkey patch - no need for concern

--- app/models/concerns/search_integration.rb
+++
@@ -1,9 +0,0 @@
-module SearchForPages
-  extend ActiveSupport::Concern
-  included do
-    include PgSearch
-    multisearchable against: :label
-  end
-end
-
-Comfy::Cms::Page.send(:include, SearchForPages)
--- config/initializers/comfy_monkey_patch.rb
+++ config/initializers/comfy_monkey_patch.rb
@@ -3,10 +3,15 @@ Rails.configuration.to_prepare do
         has_many :photos, dependent: :destroy
         has_many :resources, dependent: :destroy
         has_many :related_links, dependent: :destroy
+
+        include PgSearch
+
+        multisearchable against: :label
     end
 
     Comfy::Cms::Fragment.class_eval do
         include PgSearch
+        
         multisearchable against: :content, if: -> (record) { record.identifier == "content" }
     end
 end
