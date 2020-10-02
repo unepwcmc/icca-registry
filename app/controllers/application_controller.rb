dbc6433efa9f60616bb1352179834dcd3440f515
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-02 11:34:05 +0100

    setting host in application controller to avoid missing url to link to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_host
 
   before_action :load_locale
   before_action :load_cms_pages
@@ -22,5 +23,9 @@ class ApplicationController < ActionController::Base
     @contact_us_page  = pages.find_by_slug_and_site_id("contact-us", @site.id)
     @about_page       = pages.where(slug: "about", site: @site).includes(:children).first
     @participate_page = pages.where(slug: "participate", site: @site).includes(:children).first
+  end
+
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.base_url
   end
 end
