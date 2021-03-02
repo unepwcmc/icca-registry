b1c2b5f93f8b606c0db88ea01f3acfd525b4b211
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-02 12:48:02 +0000

    fix: set latitude and longitude to have a maximum of 7 decimal places

--- db/migrate/20210301221411_add_precision_to_icca_site_coordinates.rb
+++ db/migrate/20210301221411_add_precision_to_icca_site_coordinates.rb
@@ -1,7 +1,7 @@
 class AddPrecisionToIccaSiteCoordinates < ActiveRecord::Migration[5.2]
   def up
-    change_column :icca_sites, :lat, :decimal, precision: 4, scale: 2
-    change_column :icca_sites, :lon, :decimal, precision: 5, scale: 2
+    change_column :icca_sites, :lat, :decimal, precision: 9, scale: 7
+    change_column :icca_sites, :lon, :decimal, precision: 10, scale: 7
   end
 
   def down 
--- db/schema.rb
+++ db/schema.rb
@@ -173,8 +173,8 @@ ActiveRecord::Schema.define(version: 2021_03_01_221411) do
     t.integer "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "lon", precision: 5, scale: 2
-    t.decimal "lat", precision: 4, scale: 2
+    t.decimal "lon", precision: 10, scale: 7
+    t.decimal "lat", precision: 9, scale: 7
   end
 
   create_table "interest_submissions", id: :serial, force: :cascade do |t|
