b4b2a89c42776835f145eff5494c541b9ee54a3c
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-02 11:11:12 +0000

    fix: show all sites on world map

--- app/assets/javascripts/comfy/admin/cms/custom.js
+++ app/assets/javascripts/comfy/admin/cms/custom.js
@@ -1,2 +1,5 @@
+// Note that these coffee scripts have to be stored here as they are used by 
+// Comfy CMS exclusively. Comfy CMS will not accept any scripts which are stored in 
+// app/javascript
 //= require 'comfy/admin/cms/resources'
 //= require 'comfy/admin/cms/editor'
